8b0aeb11f833a70588425703fceeb57e9d6b73a7
김강옥 <okkang@gimgang-og-ui-MacBookPro.local>
2024-06-02 23:30:11 +0900

    edit devops

--- src/main/java/websocket/example/chatting_server/chat/config/WebSocketConfig.java
+++ src/main/java/websocket/example/chatting_server/chat/config/WebSocketConfig.java
@@ -1,13 +1,9 @@
 package websocket.example.chatting_server.chat.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.*;
-import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocketMessageBroker
