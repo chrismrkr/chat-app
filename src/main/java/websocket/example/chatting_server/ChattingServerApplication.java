2243945d9509d1cbb55f3b4e8cccbcad7e47ae3c
chrismrkr <b635032_@daum.net>
2024-12-22 21:10:29 +0900

    edit compose

--- src/main/java/websocket/example/chatting_server/ChattingServerApplication.java
+++ src/main/java/websocket/example/chatting_server/ChattingServerApplication.java
@@ -1,5 +1,6 @@
 package websocket.example.chatting_server;
 
+import jakarta.servlet.annotation.WebFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
--- src/main/java/websocket/example/chatting_server/chatRoom/service/impl/ChatRoomServiceImpl.java
+++ src/main/java/websocket/example/chatting_server/chatRoom/service/impl/ChatRoomServiceImpl.java
@@ -3,6 +3,8 @@ package websocket.example.chatting_server.chatRoom.service.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import websocket.example.chatting_server.chat.controller.dto.ChatDto;
 import websocket.example.chatting_server.chatRoom.aop.annotation.ChatRoomHistoryLock;
